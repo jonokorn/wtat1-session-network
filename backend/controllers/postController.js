f6b93fddb00ecf5b58b850216eafd7c8c80be556
jonokorn <jonas_ost@hotmail.de>
2022-05-03 16:45:50 +0200

    Added Update and Delete Methods for the Database

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,13 +2,13 @@ const asyncHandler = require('express-async-handler')
 const Post         = require('../models/postModel')
 
 
-// Get Post -> GET /api/posts
+// Get Post -> GET /posts
 const getPost = asyncHandler(async(req,res) => {
     const posts = await Post.find()
     res.status(200).json(posts)
 })
 
-// Set Post -> POST /api/posts
+// Set Post -> POST /posts
 const setPost = asyncHandler(async(req,res) => {
     if(!req.body.text) {
         res.status(400)
@@ -22,16 +22,33 @@ const setPost = asyncHandler(async(req,res) => {
     res.status(200).json(post)
 })
 
-const updatePost = asyncHandler((req,res) => {
-    res.status(200).json({
-        message: `Update Post ${req.params.id}`
-    })
+// Update Post -> PUT /posts   
+const updatePost = asyncHandler(async(req,res) => {
+
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+        res.status(400)
+        throw new Error(`No Post found with the id ${req.params.id}`)
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id,req.body, {new: true})
+
+    res.status(200).json(updatedPost)
 })
 
-const deletePost = asyncHandler((req,res) => {
-    res.status(200).json({
-        message: `Delete Post ${req.params.id}` 
-    })
+const deletePost = asyncHandler(async (req,res) => {
+
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+        res.status(400)
+        throw new Error(`No Post found with the id ${req.params.id}`)
+    }
+
+    await post.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 
