0aea0f2c667c23c750aef1ed1c8fb127d2a09a6f
jonokorn <jonas_ost@hotmail.de>
2022-05-13 14:16:33 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,12 @@ const app               = express()
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
-app.set("view engine", "ejs")
+//Setup EJS & CSS
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
+//Usages
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(errorHandler)
@@ -18,7 +22,18 @@ app.use(errorHandler)
 //Routes
 app.use('/posts', require('./routes/postRoutes'))
 app.use('/users', require('./routes/userRoutes'))
-app.use('/', require('./routes/indexRoutes'))
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+
+app.get('/login', (req,res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req,res) => {
+    res.render('register.ejs')
+})
 
 
 connectToDatabase()
